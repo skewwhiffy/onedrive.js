c26ee3fda9f116a2b9da50290f45e80809e97dab
Kenny Hung <kennysubs+github@outlook.com>
2019-06-14 21:41:08 +0100

    Express to serve website

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 'use strict';
 
+const express = require('express');
+
+const app = express();
+const port = 38080;
+
+app.get('/api', (req, res) => res.send('This is the API'));
+app.use(express.static('resources'));
+
 /* eslint-disable no-console */
-console.log('Hello world');
+app.listen(port, () => console.log(`Listening on port ${port}`));
 /* eslint-enable no-console */
