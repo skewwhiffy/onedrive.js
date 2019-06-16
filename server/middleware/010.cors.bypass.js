93ae970446c9574fdf5c6d8048c93c3d7c9c5275
Kenny Hung <kennysubs+github@outlook.com>
2019-06-16 22:56:32 +0100

    Bypass cors only for dev

---
+++ server/middleware/010.cors.bypass.js
@@ -0,0 +1,21 @@
+'use strict';
+import autobind from 'auto-bind';
+
+export default class {
+  constructor() {
+    autobind(this);
+  }
+
+  async run(req, res, next) {
+    if (process.env.NODE_ENV === 'dev') {
+      if (!this.logged) {
+        this.logged = true;
+        const logger = await req.ioc.getLogger();
+        logger.warn('Dev environment: bypassing CORS');
+      }
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    }
+    next();
+  }
+}
