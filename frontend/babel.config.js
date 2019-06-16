099a2924caf1eeb4ce132539aa6c702dada6681b
kennysubs+github@outlook.com <kennysubs+github@outlook.com>
2019-06-16 14:54:12 +0100

    Fix frontend lint

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -2,4 +2,4 @@ export default {
   presets: [
     '@vue/app'
   ]
-}
+};
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,12 @@
-import Vue from 'vue'
+import Vue from 'vue';
 import BootstrapVue from 'bootstrap-vue';
-import App from './app.vue'
+import App from './app.vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
