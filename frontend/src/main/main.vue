ced37710934c7d7d7ca4c6438e750733d5029a84
Kenny Hung <kennysubs+github@outlook.com>
2019-06-24 19:30:54 +0100

    File vue

--- frontend/src/main/main.vue
+++ frontend/src/main/main.vue
@@ -4,17 +4,19 @@
       <b-tab title="Users" active>
         <User />
       </b-tab>
+      <b-tab title="Files">
+        <File />
+      </b-tab>
     </b-tabs>
   </div>
 </template>
 
 <script>
 import User from './user.vue';
+import File from './file.vue';
 
 export default {
   name: 'Main',
-  components: {
-    User
-  }
+  components: { User, File }
 };
 </script>
