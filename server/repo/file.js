ce480d87f6edd25625e9e76838253cf2e85316f5
Kenny Hung <kennysubs+github@outlook.com>
2019-06-24 17:34:39 +0100

    File repo, first cut

--- server/repo/file.js
+++ server/repo/file.js
@@ -1,7 +1,54 @@
 'use strict';
 
+const upsert = async (entity, where, values) => {
+  const existing = await entity.findOne({ where });
+  if (existing) {
+    await existing.update(values);
+  } else {
+    await entity.create(values);
+  }
+};
+
 export default class {
-  constructor() {
-    console.log('Hello');
+  constructor(entities) {
+    this.entities = entities;
+  }
+
+  async upsertFolder(...folders) {
+    if (!folders || folders.length === 0) return;
+    if (folders.length > 1) {
+      // TODO: Parallelize if possible
+      await this.upsertFolder(folders[0]);
+      await this.upsertFolder(...folders.slice(1));
+    }
+    const folder = folders[0];
+    const toInsert = {
+      userId: folder.userId,
+      name: folder.name,
+      id: folder.id
+    };
+    if (folder.parentFolderId) toInsert.parentFolderId = folder.parentFolderId;
+    await upsert(this.entities.Folder, { id: folder.id }, toInsert);
+  }
+
+  async getFolders({ id: userId }, path) {
+    let pathParts = path.split('/').filter(it => it);
+    let parentFolderId = null;
+    while (true) {
+      const where = {
+        userId,
+        parentFolderId
+      };
+      if (pathParts.length === 0) {
+        const response = await this.entities.Folder.findAll({ where });
+        return JSON.parse(JSON.stringify(response));
+      }
+      where.name = pathParts[0];
+      pathParts = pathParts.slice(1);
+      const response = await this.entities.Folder.findAll({ where });
+      if (response.length === 0) return [];
+      if (response.length > 1) throw Error('Too many folders');
+      parentFolderId = response[0].id;
+    }
   }
 }
--- server/repo/file.test.js
+++ server/repo/file.test.js
@@ -1,6 +1,8 @@
 'use strict';
+import _ from 'lodash';
 import shortId from 'shortid';
 import Server from '../../test.utils/integration.setup';
+import { expect } from 'chai';
 
 describe.only('File repository', () => {
   let user;
@@ -18,6 +20,37 @@ describe.only('File repository', () => {
     fileRepo = await ioc.getFileRepo();
   });
 
-  it('inserts and gets', async () => {
+  it('can insert folder at root and retrieve', async () => {
+    const folder = {
+      userId: user.id,
+      name: shortId(),
+      id: shortId()
+    };
+
+    await fileRepo.upsertFolder(folder);
+    const fromRepo = await fileRepo.getFolders(user, '/');
+
+    expect(fromRepo).to.have.length(1);
+  });
+
+  it('can insert folder at random paths', async () => {
+    const folderEntities = [{
+      userId: user.id, name: 'folder0', id: 'folder0id'
+    }, ..._.range(2).map(it => ({
+      userId: user.id,
+      name: `folder${it + 1}`,
+      id: `folder${it + 1}id`,
+      parentFolderId: `folder${it}id`
+    }))];
+
+    await fileRepo.upsertFolder(...folderEntities);
+
+    const firstResponse = await fileRepo.getFolders(user, 'folder0');
+    const secondResponse = await fileRepo.getFolders(user, '/folder0/folder1');
+    const thirdResponse = await fileRepo.getFolders(user, 'folder0/not.a.folder');
+
+    expect(firstResponse.map(it => it.name)).to.have.members(['folder1']);
+    expect(secondResponse.map(it => it.name)).to.have.members(['folder2']);
+    expect(thirdResponse).to.be.empty();
   });
 });
