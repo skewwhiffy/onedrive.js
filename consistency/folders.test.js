b5392c662b818f91bf45ba1db1f184a441a72a24
Kenny Hung <kennysubs+github@outlook.com>
2019-06-26 19:20:25 +0100

    Folder consistency check

--- consistency/folders.test.js
+++ consistency/folders.test.js
@@ -1,33 +1,35 @@
 'use strict';
+import { expect } from 'chai';
 import { promises as fs } from 'fs';
 import untildify from 'untildify';
 import path from 'path';
-import _ from 'lodash';
 
-describe.only('Folders are consistent', () => {
+describe('Folders', function desc() {
+  this.timeout(10000);
   const existing = '~/OneDrive';
-  // const toCheck = '~/onedrive';
+  const toCheck = '~/onedrive.js';
 
-  const getFolders = async root => {
-    const rootPath = untildify(root);
-    const contents = await fs.readdir(rootPath);
-    const contentsWithLstats = await Promise.all(contents
-      .map(it => path.join(rootPath, it))
-      .map(async itemPath => ({ itemPath, lstat: await fs.lstat(itemPath) })));
-    const directSubfolders = contentsWithLstats
-      .filter(it => it.lstat.isDirectory())
-      .map(it => it.itemPath);
-    console.log(directSubfolders);
-    throw Error();
-    const subFolderCollections = await Promise.all(directSubfolders
-      .map(it => getFolders(it)));
-    const subFolders = _.flatten(subFolderCollections);
-    return [...directSubfolders, subFolders];
-  };
-
-  it('folders in existing exist in toCheck', async () => {
-    const existingFolders = await getFolders(existing);
-
-    console.log(existingFolders);
+  it('are consistent', async () => {
+    const originalQueue = [''];
+    const check = async queue => {
+      if (queue.length === 0) return;
+      const originalPath = path.join(untildify(existing), queue[0]);
+      const newPath = path.join(untildify(toCheck), queue[0]);
+      const [originalItems, newItems] = await Promise
+        .all([originalPath, newPath].map(it => fs.readdir(it)));
+      const getDirectories = async (start, items) => {
+        const promises = items
+          .map(async name => ({ name, lstat: await fs.lstat(path.join(start, name)) }));
+        const typedItems = await Promise.all(promises);
+        return typedItems.filter(it => it.lstat.isDirectory()).map(it => it.name);
+      };
+      const [originalDirectories, newDirectories] = await Promise.all([
+        getDirectories(originalPath, originalItems),
+        getDirectories(newPath, newItems)
+      ]);
+      expect(originalDirectories).to.eql(newDirectories);
+      check([...queue.slice(1), ...newDirectories.map(it => path.join(queue[0], it))]);
+    };
+    check(originalQueue);
   });
 });
